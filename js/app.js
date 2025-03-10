95470a0b459bf58865ca94005780adc68caed798
Celemm <ngolaceleste@gmail.com>
2025-03-10 16:28:11 +0100

    commit 2

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/*variables*/
+let nom = "java";
+const version = parseFloat(6.5);
+let etat = false;
+let chiffre = null;
+let trump = undefined;
+const tables = ["html", "css", true];
+const user = {
+    nom: "Doe",
+    ville: "NY",
+    age: 19
+};
+
+console.log(typeof nom);
+console.log(version);
+console.log(etat);
+console.log(typeof chiffre);
+console.log(typeof trump);
+console.log(typeof tables);
+console.log(user);
